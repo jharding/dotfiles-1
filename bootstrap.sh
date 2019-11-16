fea3bb103fdc678431e399e0f0e1c1761d1146c8
Jake Harding <j1315b@gmail.com>
2019-11-15 22:23:20 -0800

    Tweak

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,18 +1,13 @@
 #!/usr/bin/env bash
 
-cd "$(dirname "${BASH_SOURCE}")";
-
-git pull origin master;
-
 function doIt() {
 	rsync --exclude ".git/" \
 		--exclude ".DS_Store" \
-		--exclude ".osx" \
+		--exclude ".swp" \
 		--exclude "bootstrap.sh" \
 		--exclude "README.md" \
 		--exclude "LICENSE-MIT.txt" \
 		-avh --no-perms . ~;
-	source ~/.bash_profile;
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
